68bfb9ca6ee631d3ff065f07048abdd63eff1a15
Allan <fernzallan14@gmail.com>
2023-07-05 14:19:31 +0100

    tailwind-init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px", 
+    },
+    extend: {
+      colors: {
+        allan: "#F9A600",
+      },
+    },
+  },
+  plugins: [],
+};
+

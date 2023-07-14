f498fef1f3ae40d9212e231421b52376a63d7139
Allan Fernandes <Fernzallan14@gmail.com>
2023-07-14 23:02:05 +0100

    node-

---
+++ gatsby-node.js
@@ -0,0 +1,38 @@
+const { graphql } = require("gatsby");
+const path = require("path");
+
+async function turnPostsIntoPages({ graphql, actions }) {
+// 1. Get a template for this page
+  const postTemplate = path.resolve("./src/templates/Post.js");
+
+  // 2. Query all posts
+  const { data } = await graphql(`
+      query MyQuery {
+        allWpPost {
+          nodes {
+            slug
+            uri
+            id
+          }
+        }
+      }
+    `);
+
+  // 3. Loop over each post and create a page for that post
+  data.allWpPost.nodes.forEach((post) => {
+    actions.createPage({
+      // What is the URL for this new page??
+      path: post.slug,
+      component: postTemplate,
+      context: {
+        id: post.id,
+      },
+    });
+  });
+
+  console.log(data);
+}
+
+exports.createPages = async (params) => {
+  await turnPostsIntoPages(params);
+};
---
+++ src/templates/SingleBlog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SingleBlog() {
+  return (
+    <div className="mt-60">
+      <h2>THIS IS THE SINGLE BLOG PAGE</h2>
+    </div>
+  );
+}
+
+export default SingleBlog;

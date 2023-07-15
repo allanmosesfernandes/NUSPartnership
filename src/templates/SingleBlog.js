8b6d9d0b60e40f21aa4f6c69cc646ac331f3475c
Allan Fernandes <Fernzallan14@gmail.com>
2023-07-15 11:21:18 +0100

    single-blog-read-more

--- src/templates/SingleBlog.js
+++ src/templates/SingleBlog.js
@@ -1,23 +1,36 @@
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 import React from "react";
 
 function SingleBlog({ pageContext, data }) {
   // Get the blog id from the pageContext
   const { id } = pageContext;
   // Get the data from the query
-  const { title, content, date } = data.wpPost;
+  // eslint-disable-next-line react/prop-types
+  const {
+    title, content, date, featuredImage,
+  } = data.wpPost;
   // Render the blog data
   return (
-    <div className="container mt-[150px] flex w-4/5 mx-auto font-body">
-      <div className="">
-        <h2 className="text-oceanBlue text-6xl mb-5">{title}</h2>
-        <p className="text-lg italic font-bold mb-5">{date}</p>
-        <div
-          dangerouslySetInnerHTML={{ __html: content }}
-          className="wp-content-img text-lg leading-9 flex flex-col gap-2 mb-[5rem]"
-        />
+    <>
+      <GatsbyImage
+        image={featuredImage.node.gatsbyImage}
+        alt="placeholder"
+        className="w-full h-[400px] object-cover md:h-[800px] mt-[90px] md:mt-[120px]"
+      />
+      <div className="container mt-[10px] flex flex-col w-full p-4 mx-auto font-body lg:w-4/5 lg:p-6">
+        <div className="">
+          <h2 className="text-oceanBlue text-2xl mb-5 lg:text-6xl">{title}</h2>
+          <p className="line text-lg italic font-bold mb-5 flex pl-[35px] relative">
+            {date}
+          </p>
+          <div
+            dangerouslySetInnerHTML={{ __html: content }}
+            className="wp-content-img text-lg leading-9 flex flex-col gap-2 mb-[5rem]"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -27,6 +40,11 @@ export const query = graphql`
       title
       content
       date(formatString: "MMMM DD, YYYY")
+      featuredImage {
+        node {
+          gatsbyImage(fit: COVER, height: 800, layout: FULL_WIDTH)
+        }
+      }
     }
   }
 `;
